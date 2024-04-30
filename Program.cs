d04cc1ec10393049356ae92a5a81f53888eb59cf
PJS <b_rok9999@mail.ru>
2024-04-30 20:32:00 +0500

    Added another solution (wrong)

--- Program.cs
+++ Program.cs
@@ -10,20 +10,54 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
-string[] str = ["Helllo", "World", "no", "2", "Hi", "Russia", "Kazan", ":-)", "1234", "567"];
+// string[] str = ["Helllo", "World", "no", "2", "Hi", "Russia", "Kazan", ":-)", "1234", "567"];
 
-// string[] str2 = new string[str.Length];
-// for (int i = 0; i < str2.length; i++)
+// // string[] str2 = new string[str.Length];
+// // for (int i = 0; i < str2.length; i++)
+// // {
+// //     if (str[i].Length <= 3)
+// //     {
+// //         System.Console.WriteLine(str[i]);
+// //     }
+// // }
+// foreach (string w in str)
 // {
-//     if (str[i].Length <= 3)
+//     if (w.Length <= 3)
 //     {
-//         System.Console.WriteLine(str[i]);
+//         System.Console.Write($"{w} ");
 //     }
 // }
-foreach (string w in str)
+
+string[] MyToStringArray(string str)
 {
-    if (w.Length <= 3)
+    string[] words = new string[str.Length];
+    foreach (string w in words)
     {
-        System.Console.Write($"{w} ");
+        System.Console.WriteLine($"{w} ");
     }
+    // return words;
 }
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+
+string[] CreateNewArray(string[] array)
+{
+    string[] array2 = new string[array.Length];
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        array2[i].Length <= 3;
+    }
+    return array2;
+}
+
+System.Console.Write("Напишите несколько слов через пробел: ");
+string str = Console.ReadLine();
+PrintArray(MyToStringArray(str));
+PrintArray(CreateNewArray(str));
